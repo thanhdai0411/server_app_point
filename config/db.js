1aeb0c9d13a1d7d0aece3d602ab68f82dbae8bf2
thanhdai0411 <84715001+thanhdai0411@users.noreply.github.com>
2022-07-22 14:21:47 +0700

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectMongGoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.0ecuv.mongodb.net/?retryWrites=true&w=majority`);
         console.log('Connect MongoDB success!');
     } catch (error) {
         console.log(`Connect MongoDB fail :${error}`);
